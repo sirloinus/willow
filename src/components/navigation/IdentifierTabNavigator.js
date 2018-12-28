03245c420074fad96ae0c237d2cae45540d4bf5e
Amalie Smidth <amaliesmidth@gmail.com>
2018-12-28 14:41:40 +0000

    added swipeout library to location markers list

---
+++ src/components/navigation/IdentifierTabNavigator.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { createBottomTabNavigator } from 'react-navigation'
+
+import IdentifierScreenStackNavigator from './IdentifierScreenStackNavigator'
+import LibraryScreen from '../screens/LibraryScreen'
+
+const IdentifierTabNavigator = createBottomTabNavigator(
+    {
+        Camera: { screen: IdentifierScreenStackNavigator },
+        Library: { screen: LibraryScreen }
+    }, {
+        navigationOptions: ({ navigation }) => ({
+            tabBarIcon: ({ focused, tintColor }) => {
+                const { routeName } = navigation.state
+            }
+        }),
+        tabBarOptions: {
+            activeTintColor: 'rgb(0, 196, 124)',
+            inactiveTintColor: 'white',
+            labelStyle: {
+                fontSize: 15
+            },
+
+            style: {
+                backgroundColor: 'rgba(0, 0, 0, 0.9)',
+                // borderTopWidth: 3,
+                // borderTopColor: 'rgb(92, 162, 111)',
+                // position: 'absolute',  
+                left: 0,
+                right: 0,
+                bottom: 0
+            }
+        }
+    }
+)
+
+export default IdentifierTabNavigator
